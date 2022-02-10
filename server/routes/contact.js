fddc43083eb0515682d7bbccdeba422ce0404b75
vantohuu <82772386+vantohuu@users.noreply.github.com>
2022-02-11 01:11:11 +0700

    Update contact.js

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -56,7 +56,7 @@ router.delete('/delete', async(req, res) => {
       return res.status(200).json({ success: true, message: 'Deleted')
   } catch (error) {
       console.log(error)
-  return res.status(400).json({ success: false, message: Wrong' })
+  return res.status(400).json({ success: false, message: 'Wrong' })
  }
 })
 module.exports = router;
